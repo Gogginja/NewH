e5665b4a5dbbad5bd66ffd69f1a1c059f5726e44
Gogginja <89789694+Gogginja@users.noreply.github.com>
2022-03-20 23:26:24 -0400

    converted to modules

--- coin.py
+++ coin.py
@@ -1,13 +1,33 @@
+from pickle import FALSE, TRUE
 import pygame
-
+import player
 coin_image = pygame.image.load('Image/coin11.png')
 coin = [
     pygame.Rect(600, 150, 22, 22),
     pygame.Rect(200, 250, 22, 22)
     ]
-
+RED = (237, 12, 12)
 goal = pygame.Rect(350, 0, 10, 30)
 
-def makeCoin():
+score = 0
+
+def collect(x, y):
+    player_x = x
+    player_y = y
+    player_rect = pygame.Rect(player_x, player_y,
+                              player.player_width, player.player_height)
     for c in coin:
-        screen.blit(coin_image, (c[0], c[1]))
+        if c.colliderect(player_rect):
+            coin.remove(c)
+            return 1
+        else:
+            return 0
+def end(x, y):
+    player_x = x
+    player_y = y
+    player_rect = pygame.Rect(player_x, player_y,
+                              player.player_width, player.player_height)
+    if goal.colliderect(player_rect):       
+        return FALSE
+    else:
+        return TRUE
--- screen.py
+++ screen.py
@@ -1,21 +1,40 @@
-from pickle import TRUE
+from pickle import FALSE, TRUE
+from tkinter import Y
 import pygame
 import platform
 import player
+import coin
 class screen:
     SCREEN_SIZE = (700, 500)
     DARK_GREY = (50, 50, 50)
     GREEN = (16, 173, 42)
+    clock = pygame.time.Clock()
+    game_state = 'play'
     
     run  = TRUE
-    while run:
+    while run == TRUE:
         pygame.init()
         screen = pygame.display.set_mode(SCREEN_SIZE)
         screen.fill(DARK_GREY)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        for c in coin.coin:
+            screen.blit(coin.coin_image, (c[0], c[1]))
+        pygame.draw.rect(screen, coin.RED, coin.goal)
         screen.blit(player.player_image, (player.player_x, player.player_y))
+        keys = pygame.key.get_pressed()
+        player.player_x = player.movementHorizantal(keys, player.player_x, player.player_y, 
+                                                    player.player_width, player.player_height)
+        fall = player.movementVertical(keys, player.player_x, player.player_y, 
+                                       player.player_width, player.player_height, player.player_speed, player.ground)
+        coin.score += coin.collect(player.player_x,player.player_y)
+        # End Goal
+        run = coin.end(player.player_x, player.player_y)
+        player.player_y = fall[0]
+        player.player_speed = fall[1]
+        player.ground = fall[2]
         platform.makePlatform(screen,platform.platforms,GREEN)
         pygame.display.flip()
+        clock.tick(60)
     pygame.quit()
