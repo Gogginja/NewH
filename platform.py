b417eb249f2a01c71532954e7747aea3465a6779
HarrisonCorbin2021 <90351867+HarrisonCorbin2021@users.noreply.github.com>
2022-04-01 12:58:09 -0400

    Added the layout of 2 new levels, 3 is WIP

--- platform.py
+++ platform.py
@@ -17,6 +17,29 @@ platforms = [
     pygame.Rect(300, 100, 25, 50)
 ]
 
+level1 = [pygame.Rect(0, 450, 700, 50)]
+
+level2 = [
+            pygame.Rect(0, 450, 700, 50),
+            pygame.Rect(0, 200, 100, 400),
+            pygame.Rect(100, 200, 75, 25),
+            pygame.Rect(300, 100, 120, 20),
+            pygame.Rect(300, 175, 120, 20),
+            pygame.Rect(300, 250, 120, 20),
+            pygame.Rect(500, 100, 120, 20),
+            pygame.Rect(500, 175, 120, 20),
+            pygame.Rect(150, 350, 100, 100),
+            pygame.Rect(450, 300, 100, 200),
+            pygame.Rect(550, 375, 120, 10)
+         ]
+level3 = [
+            pygame.Rect(0, 450, 700, 50)
+         ]
+slopes = [(200,200),(200,250),(300,200),(300,150)]
+
+
 def makePlatform(screen,arr, color):
+    #Test for an upward incline using polygons
+    #pygame.draw.polygon(screen,color,slopes)
     for p in arr:
         pygame.draw.rect(screen,color,p)
