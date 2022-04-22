d1f4d4e2f8b95a859c2a19197b6e5cacdc4239d7
corbinh <corbinh@mail.gvsu.edu>
2022-04-21 23:22:49 -0400

    main testing

--- test_main.py
+++ test_main.py
@@ -1,34 +1,40 @@
 import unittest
 import main
+import coin
+import platform
+import player
 
 class TestMain(unittest.TestCase):
-    #Tests to see if the game can be paused
+    #Tests to see if the game is paused
     def test_pause(self):
-        self.assertEquals(main.getPaused(), True)
-    #Tests to see if the game can be paused
+        self.assertEquals(main.paused, True)
+    #Tests to see if the game is in the Main Menu
     def test_mainMenu(self):
-        self.assertEquals(main.getScreen(), 'mainMenu')
-
+        self.assertEquals(main.currentScreen, 'mainMenu')
+    #Tests to see if the game is in the level select
     def test_select(self):
-        self.assertEquals(main.getScreen(), 'levelSelect')
-
+        self.assertEquals(main.currentScreen, 'level_select')
+    #Tests to see if the game is in the settings menu
     def test_settings(self):
-        self.assertEquals(main.getScreen(), 'settings')
-
+        self.assertEquals(main.currentScreen, 'settings')
+    #Tests to see if the game is in the audio settings
     def test_audio(self):
-        self.assertEquals(main.getScreen(), 'settings_audio')
-
+        self.assertEquals(main.currentScreen, 'settings_audio')
+    #Tests to see if the game is in the gameplay
     def test_gameplay(self):
-        self.assertEquals(main.getScreen(), 'settings_gameplay')
-
+        self.assertEquals(main.currentScreen, 'settings_gameplay')
+    #Tests to see if the game is in the video settings
     def test_video(self):
-        self.assertEquals(main.getScreen(), 'settings_video')
-
+        self.assertEquals(main.currentScreen, 'settings_video')
+    #Tests to see if the game is in Level 1
     def test_level1Active(self):
-        self.assertEquals(main.getScreen(), 'level_1')
-    
+        self.assertEquals(main.currentScreen, 'level_1')
+    #Tests to see if the game is in Level 2
     def test_level2Active(self):
-        self.assertEquals(main.getScreen(), 'level_2')
-
+        self.assertEquals(main.currentScreen, 'level_2')
+    #Tests to see if the game is in Level 3
     def test_level3Active(self):
-        self.assertEquals(main.getScreen(), 'level_3')
+        self.assertEquals(main.currentScreen, 'level_3')
+
+if __name__ == '__main__':
+    unittest.main()
