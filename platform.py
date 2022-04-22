ff61361f26786852c5f479f160e6a3dd1e2048ce
Jack Kleitch <kleitjac@mail.gvsu.edu>
2022-04-21 22:18:52 -0400

    Removed useless comment

--- platform.py
+++ platform.py
@@ -72,38 +72,3 @@ def makePlatform(screen,arr, color):
     #pygame.draw.polygon(screen,color,slopes)
     for p in arr:
         pygame.draw.rect(screen,color,p)
-        
-'''
-levelTest = [
-    [0, 450, 50, 50],
-    [100, 450, 50, 50],
-    [200, 450, 50, 50],
-    [350, 450, 50, 50],
-    [400, 400, 50, 50],
-    [450, 350, 50, 50],
-    [500, 300, 50, 50],
-    [550, 250, 50, 50],
-    [600, 200, 50, 50],
-    [450, 200, 50, 50],
-    [250, 300, 100, 25],
-    [300, 150, 100, 25],
-    [300, 100, 25, 50]
-]
-
-def level1Test(screen,color, offset):
-    #Test for an upward incline using polygons
-    #pygame.draw.polygon(screen,color,slopes)
-        pygame.draw.rect(screen,color,pygame.Rect(levelTest[0][0]-offset, levelTest[0][1], levelTest[0][2], levelTest[0][3]))
-        pygame.draw.rect(screen,color,pygame.Rect(levelTest[1][0]-offset, levelTest[1][1], levelTest[1][2], levelTest[1][3]))
-        pygame.draw.rect(screen,color,pygame.Rect(levelTest[2][0]-offset, levelTest[2][1], levelTest[2][2], levelTest[2][3]))
-        pygame.draw.rect(screen,color,pygame.Rect(levelTest[3][0]-offset, levelTest[3][1], levelTest[3][2], levelTest[3][3]))
-        pygame.draw.rect(screen,color,pygame.Rect(levelTest[4][0]-offset, levelTest[4][1], levelTest[4][2], levelTest[4][3]))
-        pygame.draw.rect(screen,color,pygame.Rect(levelTest[5][0]-offset, levelTest[5][1], levelTest[5][2], levelTest[5][3]))
-        pygame.draw.rect(screen,color,pygame.Rect(levelTest[6][0]-offset, levelTest[6][1], levelTest[6][2], levelTest[6][3]))
-        pygame.draw.rect(screen,color,pygame.Rect(levelTest[7][0]-offset, levelTest[7][1], levelTest[7][2], levelTest[7][3]))
-        pygame.draw.rect(screen,color,pygame.Rect(levelTest[8][0]-offset, levelTest[8][1], levelTest[8][2], levelTest[8][3]))
-        pygame.draw.rect(screen,color,pygame.Rect(levelTest[9][0]-offset, levelTest[9][1], levelTest[9][2], levelTest[9][3]))
-        pygame.draw.rect(screen,color,pygame.Rect(levelTest[10][0]-offset, levelTest[10][1], levelTest[10][2], levelTest[10][3]))
-        pygame.draw.rect(screen,color,pygame.Rect(levelTest[11][0]-offset, levelTest[11][1], levelTest[11][2], levelTest[11][3]))
-        pygame.draw.rect(screen,color,pygame.Rect(levelTest[12][0]-offset, levelTest[12][1], levelTest[12][2], levelTest[12][3]))
-'''
